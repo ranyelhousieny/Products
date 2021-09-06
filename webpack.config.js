7991cc82e5904155a7674aaf127d7c232bc428e8
Rany ElHousieny <elhousie@gmail.com>
2021-09-06 12:57:38 -0700

    Step 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
 module.exports = {
   mode: 'development',
+  devServer: {
+    port: 8001,
+  },
   module: {
     rules: [
       {
@@ -32,6 +37,17 @@ module.exports = {
     ],
   },
   plugins: [
+    new ModuleFederationPlugin(
+      {
+        name: 'ProductsMicrofrontend',
+        filename:
+          'remoteEntry.js',
+        exposes: {
+          './ProductsIndex':
+            './src/index.js',
+        },
+      }
+    ),
     new HtmlWebpackPlugin({
       template:
         './public/index.html',
