d40f5136b8bd0004908216c03cf42a00fb4f045a
Rany ElHousieny <elhousie@gmail.com>
2021-10-12 20:05:50 -0700

    2

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,22 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 
-const template = `
-  <div>
-    <h1>===========</h1>
-    <h3>
-      Products Microfrontend
-    </h3>
+import App from './App';
 
-    <ul>
-      <li>Product1</li>
-      <li>Product2</li>
-      <li>Product3</li>
-    </ul>
-    <h1>===========</h1>
-  </div>
-`;
-
-document.querySelector(
-  '#fromjs'
-).innerHTML = template;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById(
+    'fromjs'
+  )
+);
